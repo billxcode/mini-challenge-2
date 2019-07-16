92a0953a47f4a17f38ed4d31b4ef0a3ed308b009
Idris <idrisnovandi@gmail.com>
2019-07-16 16:15:26 +0700

    add pose detection

---
+++ Sprintastic/Model/PredictedPoint.swift
@@ -0,0 +1,13 @@
+//
+//  PredictedPoint.swift
+//  Sprintastic
+//
+//  Created by Idris on 16/07/19.
+//  Copyright © 2019 IdrisLabs. All rights reserved.
+//
+import CoreGraphics
+
+struct PredictedPoint {
+    let maxPoint: CGPoint
+    let maxConfidence: Double
+}
