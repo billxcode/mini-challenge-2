8fd5b5ca6f85340959b5fefca6781a7b99273fef
Idris <idrisnovandi@gmail.com>
2019-07-22 00:48:28 +0700

    add icon hint

--- Sprintastic/Controller/PhaseDetailViewController.swift
+++ Sprintastic/Controller/PhaseDetailViewController.swift
@@ -44,15 +44,4 @@ class PhaseDetailViewController: UIViewController {
         titleSupportingLabel.text = phaseData.titleSupporting
         descriptionSupportingLabel.text = phaseData.descriptionSupporting
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
